fbce6b3a6e37706dc62f6dc317b0cc9b03d83b24
Brandon Muteau <muteaub@gmail.com>
2022-06-01 17:50:35 +0200

    project in progress

---
+++ src/app/pokemon/pokemon.service.ts
@@ -0,0 +1,49 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, of, tap } from 'rxjs';
+import { Pokemon } from './pokemon';
+
+@Injectable()
+export class PokemonService {
+
+  constructor(private http: HttpClient)  { }
+
+  getPokemonList(): Observable <Pokemon[]> {
+    return this.http.get<Pokemon[]>('api/pokemons').pipe(
+      tap((response) =>this.log(response)),
+      catchError((error)=> this.handleError(error,[]))
+    );
+  }
+
+  getPokemonById(pokemonId: number): Observable<Pokemon|undefined>{
+    return this.http.get<Pokemon>(`api/pokemon/${pokemonId}`).pipe(
+      tap((response) =>this.log(response)),
+      catchError((error)=> this.handleError(error,undefined))
+    );
+  }
+
+  private log(response: Pokemon[]|Pokemon|undefined) {
+    console.table(response);
+  }
+
+  private handleError(error: Error, errorValue: any) {
+    console.error(error);
+    return of(errorValue);
+  }
+
+  getPokemonTypeList(): string[]{
+    return [
+      'Plante',
+      'Feu',
+      'Eau',
+      'Insecte',
+      'Normal',
+      'Electrik',
+      'Poison',
+      'Fée',
+      'Vol',
+      'Combat',
+      'Psy'
+    ];
+  }
+}
